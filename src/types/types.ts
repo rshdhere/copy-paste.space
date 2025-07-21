a560b10ac737baa078a58ec0b5f86b1158d0535b
raashed <mohammedraashed10@gmail.com>
2025-07-21 09:50:43 +0530

    feature: updating the json-message

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,6 +5,6 @@ export const ContentSchema = z.object({
 });
 
 export const OTPSchema = z.object({
-    userCode: z.string({message: "invalid input format or use only 'userCode' for GET request"}).length(6, {message: "user is allowed to send only 6-digit code"}).regex(/^[A-Z0-9]+$/, {message: "you can only enter numbers along with upper-case alphabets"})
+    userCode: z.string({message: "invalid input format or use only 'userCode' for GET request"}).length(6, {message: "user is allowed to send only 6-digit code"}).regex(/^[A-Z0-9]+$/, {message: "user can only enter numbers along with upper-case alphabets"})
 
 })
