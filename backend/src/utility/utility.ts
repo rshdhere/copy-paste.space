d16f2a889a3e2fca0bcad40bf2bd60885ee06988
raashed <mohammedraashed10@gmail.com>
2025-07-25 23:40:28 +0530

    feat: upgrading utility to 4

--- backend/src/utility/utility.ts
+++ backend/src/utility/utility.ts
@@ -1,9 +1,9 @@
 export function random( len: number ){
-    let options = process.env.OPTIONS;
+    let options = process.env.OPTIONS ?? "abcd1234";
     let ans = '';
 
-    for (let i = 0; i < 6; i++){
-        ans += options![Math.floor((Math.random() * 6))]
+    for (let i = 0; i < len; i++) {
+        ans += options![Math.floor(Math.random() * options.length)];
     }
     return ans;
 }
