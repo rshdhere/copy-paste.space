2ed8cd0889b67bcfa51043d782a5d61b2137837c
raashed <mohammedraashed10@gmail.com>
2025-07-25 19:33:23 +0530

    bug: input must be just string

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -1,7 +1,10 @@
 import { z } from "zod"
 
 export const ContentSchema = z.object({
-    content: z.string({message: "invalid input format or use only 'content' for POST request"}).regex(/^[a-zA-Z0-9\s\u00C0-\u017F\u0100-\u024F\u1E00-\u1EFF.,!?;:()\-_'"\/@#$%&*+=\[\]{}|\\~`\n\r\t\u2000-\u206F\u2E00-\u2E7F\u3000-\u303F\uFE10-\uFE1F\uFE30-\uFE4F]{1,10000}$/, {message: "user can send only code-text-languages etc, vulnarabilities like SQL-Injections must fail"})
+    content: z.string({message: "invalid input format or use only 'content' for POST request"})
+    // .regex(/^[a-zA-Z0-9\s\u00C0-\u017F\u0100-\u024F\u1E00-\u1EFF.,!?;:()\-_'"\/@#$%&*+=\[\]{}|\\~`\n\r\t\u2000-\u206F\u2E00-\u2E7F\u3000-\u303F\uFE10-\uFE1F\uFE30-\uFE4F]{1,10000}$/, {message: "user can send only code-text-languages etc, vulnarabilities like SQL-Injections must fail"})
+    
+    // need to fix this area
 });
 
 export const OTPSchema = z.object({
