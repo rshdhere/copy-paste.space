bbe0072899d333292acb0aee926cd9c942b6cedf
raashed <mohammedraashed10@gmail.com>
2025-07-25 19:34:18 +0530

    bug: added cors to server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { ConnectedToDB } from "./database/database";
 import senderRouter from "./routes/sender";
 import receiverRouter from "./routes/receiver";
@@ -6,6 +7,7 @@ const app = express();
 
 // for parsing user data
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use("/api/v1/user", senderRouter);
