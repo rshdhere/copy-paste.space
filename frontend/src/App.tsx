15ae2a81473b308c4610df93728c15d6c7e16824
raashed <mohammedraashed10@gmail.com>
2025-07-25 19:41:49 +0530

    feat: react-router-dom is configured

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Sender } from "./pages/Sender";
+import { Receiver } from "./pages/Receiver";
+
 function App() {
     return (
-    <div>
-
-    </div>
-  )
+        <BrowserRouter>
+            <Routes>
+                <Route path="/send" element={<Sender />} />
+                <Route path="/receive" element={<Receiver />} />
+                <Route path="*" element={<Navigate to="/send" replace />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
